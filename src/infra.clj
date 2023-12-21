526748acb26c949c709c864484626ea1bbfd99bf
Andrea Crotti <andrea.crotti.0@gmail.com>
2023-12-21 14:39:38 +0000

    not using postgres for now

--- src/infra.clj
+++ src/infra.clj
@@ -4,21 +4,6 @@
    [clj-test-containers.core :as tc]
    [integrant.core :as ig]))
 
-
-(defn postgres []
-  (let [{:keys [postgresql]} (configuration)]
-    (-> (tc/create {:image-name    "postgres:15.3"
-                    :wait-for      {:wait-strategy :port}
-                    :exposed-ports [5432]
-                    :env-vars      {"POSTGRES_USER"        (:user postgresql)
-                                    "POSTGRES_PASSWORD"    (:password postgresql)
-                                    "POSTGRES_DB"          (:db postgresql)}})
-        #_(tc/bind-filesystem! {:host-path      "/tmp"
-                                :container-path "/opt"
-                                :mode           :read-only})
-
-        #_(tc/start!))))
-
 (defn mysql []
   (let [{:keys [mysql]} (configuration)]
     (-> (tc/create {:image-name    "mysql:8.2.0"

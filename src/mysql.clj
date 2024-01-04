f514af5b7ebf2a89977737064e8890e5811fb030
Andrea Crotti <andrea.crotti.0@gmail.com>
2024-01-04 15:27:49 +0000

    add some extra code to update etc

--- src/mysql.clj
+++ src/mysql.clj
@@ -1,5 +1,8 @@
 (ns mysql
   (:require
+   [honeysql.helpers :as hh]
+   [honeysql.core :as h]
+   [tick.core :as t]
    [next.jdbc :as j]
    [next.jdbc.sql :as js]))
 
@@ -12,24 +15,29 @@
    :password root-password
    :dbname   db})
 
+(defn update-versions [version]
+  (-> (hh/update :big_table)
+      (hh/sset {:version version, :updated_at (t/now)})
+      (h/format {:pretty true})))
+
 (defn create-table! [ds]
   (j/execute! ds
-              ["CREATE TABLE big_table (id INTEGER PRIMARY KEY, text TEXT, created_at TIMESTAMP)"]))
+              ["CREATE TABLE IF NOT EXISTS big_table (id INTEGER PRIMARY KEY, version INTEGER, text TEXT, created_at TIMESTAMP, updated_at TIMESTAMP)"]))
 
-(defn insert-data [ds]
+(defn insert-data! [ds version]
   (let [data (for [n (range 1000)]
                {:id n
                 :text (str "record: " n)
+                :created_at (t/now)
                 ;; add created at timestamp
-                })]
+                :version version})]
     (js/insert-multi! ds
                       "big_table"
                       data)
     data))
 
-(defn seed! [ds]
-  (create-table! ds)
-  (insert-data ds))
+(defn update-data! [ds version]
+  (j/execute! ds (update-versions version)))
 
 (defn get-rows [ds]
   (js/query ds ["SELECT * FROM big_table"]))
--- test/debezium_test.clj
+++ test/debezium_test.clj
@@ -7,12 +7,14 @@
    [config :refer [configuration]]
    [mysql :as m]
    [system :as s]
-   [xtdb.api :as xt]))
+   [xtdb.api :as xt]
+   [tick.core :as t]))
 
 (defn clear-files! []
   (io/delete-file (-> configuration :kafka :offsets) true)
   (io/delete-file (-> configuration :kafka :schema-history) true))
 
+;; what kind of data do I want to enter and query back?
 
 (deftest msyql->xtdb-test
   (testing "Can sync data from one db to the other"
@@ -23,16 +25,29 @@
       (let [port   (-> system :infra/mysql :port)
             config (-> configuration :mysql)
             ds     (m/get-ds config port)
-            node   (-> system :xtdb/node)
-            _data   (m/seed! ds)]
+            node   (-> system :xtdb/node)]
 
+        ;; do a first seeding of the data
+        (m/create-table! ds)
+        (m/insert-data! ds 1)
         ;; would be nice to avoid this sleep, but the retry alone is not enough
         (Thread/sleep 3000)
         ;; now check that the data is in XTDB
         (die/with-retry
             {:max-retries 100
              :delay-ms    100}
-          (is (= {:text "record: 0", :created_at nil, :xt/id 0}
-                 (xt/entity (xt/db node) 0)))))
+          (is (= {:text "record: 0", :xt/id 0, :version 1}
+                 ;; use the entity history here
+                 (-> (xt/entity (xt/db node) 0)
+                     ;; don't need to check the timestamps for now
+                     (select-keys [:text :xt/id :version])))))
+        ;; now seed again to see if there are multiple versions
+        (is (= 1 (count (xt/entity-history (xt/db node) 0 :asc))))
+        (m/update-data! ds 2)
+        ;; try to avoid sleeps if possible
+        (Thread/sleep 3000)
+        (is (= 2 (count (xt/entity-history (xt/db node) 0 :asc)))))
       (finally
         (s/stop-all)))))
+
+;; write the same value multiple times and see the different content?
